cd876f622b7437953d45519495b27282401fe8f0
Vincent <20222005026@m.scnu.edu.cn>
2025-07-08 00:14:04 +0800

    feat: 添加单点文件上传接口，支持指定存储桶上传

--- common/minio-util/src/main/java/cn/edu/scnu/danmakutv/minio/controller/UploadController.java
+++ common/minio-util/src/main/java/cn/edu/scnu/danmakutv/minio/controller/UploadController.java
@@ -4,11 +4,20 @@ import cn.edu.scnu.danmakutv.minio.service.impl.MinioService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
     @Resource
     private MinioService minioService;
+
+    // 单点上传
+    @PostMapping("/single")
+    public String uploadFile(@RequestParam("file") MultipartFile file,
+                             @RequestParam("bucketName") String bucketName) throws Exception {
+        return minioService.uploadFile(file, bucketName);
+    }
 }
