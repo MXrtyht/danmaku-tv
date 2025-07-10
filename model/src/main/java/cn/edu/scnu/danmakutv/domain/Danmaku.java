abdf48e4279ffad31c0974c160bdd9ad95fe48d2
Vincent <20222005026@m.scnu.edu.cn>
2025-07-10 16:36:20 +0800

    feat: 添加弹幕表及其实体类

---
+++ model/src/main/java/cn/edu/scnu/danmakutv/domain/Danmaku.java
@@ -0,0 +1,29 @@
+package cn.edu.scnu.danmakutv.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_danmaku")
+public class Danmaku {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    @TableField("user_id")
+    private Long userId;
+
+    @TableField("video_id")
+    private Long videoId;
+
+    @TableField("content")
+    private String content;
+
+    @TableField("create_at")
+    private LocalDateTime createAt;
+
+}
--- src/main/resources/sql/interaction.sql
+++ src/main/resources/sql/interaction.sql
@@ -3,10 +3,23 @@ USE `danmaku-tv`;
 DROP TABLE IF EXISTS `t_video_like`;
 CREATE TABLE `t_video_like`
 (
-    `id`          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `user_id`     BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
-    `video_id`    BIGINT UNSIGNED NOT NULL COMMENT '视频ID',
-    `create_at`   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '点赞时间',
+    `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `user_id`   BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
+    `video_id`  BIGINT UNSIGNED NOT NULL COMMENT '视频ID',
+    `create_at` DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '点赞时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='视频点赞表';
+
+DROP TABLE IF EXISTS `t_danmaku`;
+CREATE TABLE `t_danmaku`
+(
+    `id`           BIGINT UNSIGNED                                              NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `user_id`      BIGINT UNSIGNED                                              NOT NULL COMMENT '用户ID',
+    `video_id`     BIGINT UNSIGNED                                              NOT NULL COMMENT '视频ID',
+    `content`      TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci        NOT NULL COMMENT '弹幕内容',
+    `create_at`    DATETIME                                                     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 52
+  DEFAULT CHARSET = utf8mb4 COMMENT ='弹幕表';
