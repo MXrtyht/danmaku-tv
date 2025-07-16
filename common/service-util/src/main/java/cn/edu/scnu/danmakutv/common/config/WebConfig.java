5a94a980260beafe232bdffcf4b35bee78b7a103
Vincent <20222005026@m.scnu.edu.cn>
2025-07-17 02:42:04 +0800

    feat: 全局配置跨域, 放行跨域请求

---
+++ common/service-util/src/main/java/cn/edu/scnu/danmakutv/common/config/WebConfig.java
@@ -0,0 +1,24 @@
+package cn.edu.scnu.danmakutv.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 允许所有路径
+                        .allowedOriginPatterns("*") // 允许所有前端域名
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 允许带 cookie
+            }
+        };
+    }
+}
+
